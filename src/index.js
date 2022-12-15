317cfcf0fec95d46d945b129f402a0e2dcb97183
Joanna <astronautnemo@gmail.com>
2022-12-15 19:11:47 +0300

    fetch and display anime on display-card

--- src/index.js
+++ src/index.js
@@ -1,26 +1,33 @@
 document.addEventListener('DOMContentLoaded', () => {
+    /* Event Listeners */
+
+    /* Fetch Commands */
     fetch('https://api.jikan.moe/v4/anime/1')
     .then((response) => response.json())
     .then((data) => displayOnCard(data.data));
 })
 
-// Fetch commands
 
-
-// DOM manipulation
+/* DOM Manipulation*/
 function displayOnCard(animeData){
     console.log(animeData.images.jpg.small_image_url)
     /* POSTER SIDE */
     // Set #anime-poster
     const animePoster = document.getElementById('anime-poster');
-    animePoster.setAttribute('src', animeData.images.jpg.large_image_url)
+    animePoster.setAttribute('src', animeData.images.jpg.large_image_url);
 
     /* RIGHT SIDE */
     // Set #title
+    const animeTitle = document.getElementById('title');
+    animeTitle.innerText = animeData.title_english;
 
     // Set #jap-title
+    const animeJapTitle = document.getElementById('jap-title');
+    animeJapTitle.innerText = animeData.title_japanese;
 
     // Set #anime-synopsis
+    const animeSynopsis = document.getElementById('anime-synopsis');
+    animeSynopsis.innerText = animeData.synopsis;
 }
 
 function addToWatchlist(){
