9dc57f5015709f70a53c352832a219419b4fe82b
Joanna <astronautnemo@gmail.com>
2022-12-15 22:04:03 +0300

    separated fetch and display from Jikan and fetch and display from local

--- src/index.js
+++ src/index.js
@@ -4,21 +4,37 @@ const addToWLBtn = document.getElementById('add-btn');
 
 // DOM Content Loaded
 document.addEventListener('DOMContentLoaded', () => {
+    console.log('page loaded')
     /* Event Listeners */
-    
     addToWLBtn.addEventListener('click', () => addToWatchlist());
+
+    /* Function Calls */
+    disableWatchlistItems();
 
     /* Fetch Commands */
     // Fetch Cowboy Bebop and display on site load
-    fetch('https://api.jikan.moe/v4/anime/1')
-    .then((response) => response.json())
-    .then((data) => displayOnCard(data.data));
+    // fetchFromJikan('https://api.jikan.moe/v4/anime/1')
+
+    fetchFromLocal('http://localhost:3000/watchlist/1')
 })
+
+/* FETCH REQUESTS */
+function fetchFromJikan(URL){
+    fetch(URL)
+    .then((response) => response.json())
+    .then((data) => displayFromJikanOnCard(data.data));
+}
+
+function fetchFromLocal(URL){
+    fetch(URL)
+    .then((response) => response.json())
+    .then((data) => displayFromLocalOnCard(data));
+}
 
 
 /* DOM Manipulation*/
 // Display Card Manipulation
-function displayOnCard(animeData){
+function displayFromJikanOnCard(animeData){
     // console.log(animeData)
     /* POSTER SIDE */
     // Set #anime-poster
@@ -40,17 +56,56 @@ function displayOnCard(animeData){
     animeSynopsis.innerText = animeData.synopsis;
 }
 
+function displayFromLocalOnCard(animeData){
+    console.log(animeData)
+    /* POSTER SIDE */
+    // Set #anime-poster
+    const animePoster = document.getElementById('anime-poster');
+    animePoster.setAttribute('src', animeData.animePoster);
+
+    /* RIGHT SIDE */
+    // Set #title
+    const animeTitle = document.getElementById('title');
+    animeTitle.innerText = animeData.animeTitle;
+    animeTitle.setAttribute('alt', animeData.id);
+
+    // Set #jap-title
+    const animeJapTitle = document.getElementById('jap-title');
+    animeJapTitle.innerText = animeData.animeJapTitle;
+
+    // Set #anime-synopsis
+    const animeSynopsis = document.getElementById('anime-synopsis');
+    animeSynopsis.innerText = animeData.animeSynopsis;
+}
+
 // Watchlist Manipulation
 function populateWatchlist(){
     // Check db.json and update each list item based on the watchlist content
 }
 
 function addToWatchlist(){
-    // POST request to local server
-
     // Create list item
     disableWatchlistItems();
     createWatchlistItem();
+
+    // Create object of information to store
+    const animeDetails = {
+        animePoster: document.getElementById('anime-poster').getAttribute('src'),
+        animeTitle: document.getElementById('title').innerText,
+        animeJapTitle: document.getElementById('jap-title').innerText,
+        animeSynopsis: document.getElementById('anime-synopsis').innerText,
+    }
+
+    // POST request to local server (save only the information you display)
+    // fetch("http://localhost:3000/watchlist", {
+    //     method: 'POST',
+    //     headers: {
+    //         'Content-Type': 'application/json',
+    //         Accept: 'application/json'
+    //     },
+    //     body: JSON.stringify(animeDetails)
+    // })
+    // .then((response) => response.json()).then((data) => console.log(data))
 }
 
 function createWatchlistItem(){
@@ -63,22 +118,20 @@ function createWatchlistItem(){
         listItem.setAttribute('class', 'item active');
 
         // Display the anime's information
+        // Fetch from local db, then call displayOnCard based on id
 
-        console.log(listItem)
     })
 
     listItem.setAttribute('id', document.getElementById('title').getAttribute('alt'));
     listItem.setAttribute('class', 'item active');
     listItem.innerText = document.getElementById('title').innerText;
-    // listItem.innerText = capitaliseFirstLetter(document.getElementById('title').innerText);
-
 
     (document.getElementById('watchlist')).appendChild(listItem);
 }
 
 // Disable all the watchlist items
 function disableWatchlistItems(){
-    Array.from(document.getElementsByClassName('item')).forEach(item => console.log(item.setAttribute('class', 'item')))
+    Array.from(document.getElementsByClassName('item')).forEach(item => item.setAttribute('class', 'item'))
 }
 // Capitalise only the first letter of every word in the title
 // function capitaliseFirstLetter(string){
